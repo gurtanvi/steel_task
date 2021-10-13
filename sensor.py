5b6837a2e2a0eb60b7a5f477f3a94e652e476ff1
gurtanvi <gurtanvi@gmail.com>
2021-10-13 14:49:37 +0200

    modified sensor.py

--- sensor.py
+++ sensor.py
@@ -2,32 +2,24 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from xgboost import XGBClassifier
-from xgboost import plot_importance
+from xgboost import XGBClassifier, plot_importance
 from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
 np.random.seed(seed = 42)
 
 #Reading the dataset
-train_data = '/home/gurleenkaur/steel_task/steel_task/task_data.csv'
+train_data = './task_data.csv'
 with open(train_data, mode = 'r') as f:
     data = pd.read_csv(f)
+
+
 
 headers = np.array(list(data.columns.values))
 feature_names = headers[2:]
 class_labels = data.class_label
 feature_values = pd.DataFrame(data,columns=feature_names)
-
-#Exploring the dataset
-print(data.describe())
-print(data.head())
 
 #Exploring the dataset by visualizing the data to see if there is any correlation between the features
 sns.heatmap(feature_values.assign(target = class_labels).corr().round(2), cmap = 'Blues', annot = True).set_title('Correlation matrix', fontsize = 14)
@@ -58,7 +50,6 @@ def plot_imp_features(imp_features_df, title):
        .set_title(title, fontsize = 20)
 
 imp_features_table = imp_features_df(feature_values.columns, rf.feature_importances_)
-print(imp_features_table)
 
 plot_imp_features(imp_features_table, 'Default feature importance (scikit-learn)')
 plt.show()
@@ -70,8 +61,6 @@ xgb = XGBClassifier(n_estimators = 100,
                            oob_score = True,
                            bootstrap = True, random_state=42)
 xgb.fit(feature_values, class_labels)
-# feature importance
-print(xgb.feature_importances_)
 
 # plot using default scikit learn feature selection 
 plt.bar(range(len(xgb.feature_importances_)), xgb.feature_importances_)
